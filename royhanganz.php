a96a0516a439bbabe0f0cf997e84197c65e89714
yogiepohan <pohanyogie2011@gmail.com>
2023-05-25 13:58:29 +0700

    coba1

--- royhanganz.php
+++ royhanganz.php
@@ -1,5 +1,12 @@
-<?php
-echo "Halo Yogie Kontol";
-echo "Halo Royhan MEMEK";
-echo "Halo Yogie Kontol";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Royhan kontol</h1>
+</body>
+</html>
